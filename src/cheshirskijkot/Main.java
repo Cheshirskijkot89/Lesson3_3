920de7586ffc77cb708722886eb6938651e51fa2
Andrij Chizhenko <chizh.and.ol@gmail.com>
2018-10-14 17:07:49 +0300

    1st commit

---
+++ src/cheshirskijkot/Main.java
@@ -0,0 +1,29 @@
+package cheshirskijkot;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int year;
+
+		System.out.println("Please enter the year");
+
+		year = sc.nextInt();
+		sc.close();
+
+		if (year % 400 == 0) {
+			System.out.println("leap year");
+		} else if (year % 100 == 0) {
+			System.out.println("non-leap year");
+		} else if (year % 14 == 0) {
+			System.out.println("leap year");
+		} else {
+			System.out.println("non-leap year");
+		}
+
+	}
+
+}
